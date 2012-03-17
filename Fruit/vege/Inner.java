ac3d633a808bbd9dda62eb5a19ab4785f4f3d028
li yang <liyang1031@gmail.com>
2012-03-17 19:41:02 +0800

    add Fruit for initialization

---
+++ Fruit/vege/Inner.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vege;
+
+/**
+ *
+ * @author Octobershiner
+ */
+public class Inner {
+    public Inner(){
+        System.out.println("Inner Constructor");
+    }
+}
